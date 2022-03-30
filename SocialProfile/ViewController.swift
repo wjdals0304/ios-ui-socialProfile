1bdb6dcd9c5b50b761fbc3567e899fd042644575
jmkim <wjdals0304@gmail.com>
2022-03-30 15:30:31 +0900

    Initial Commit

---
+++ SocialProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialProfile
+//
+//  Created by 김정민 on 2022/03/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
